4148bdcb11cb46c3046993d60b50b6a931fcbb6f
mnino80-rgb <mnino80@gmail.com>
2026-03-11 07:44:00 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,13 +15,11 @@ namespace OWAHelper
             {
                 string appName = Path.GetFileName(Application.ExecutablePath);
 
-                RegistryKey key = Registry.CurrentUser.CreateSubKey(
-                @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION");
-
-                if (key != null)
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(
+                    @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION"))
                 {
-                    key.SetValue(appName, 11001, RegistryValueKind.DWord);
-                    key.Close();
+                    if (key != null)
+                        key.SetValue(appName, 11001, RegistryValueKind.DWord);
                 }
             }
             catch { }
@@ -35,30 +33,6 @@ namespace OWAHelper
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            string url = null;
-
-            if (args.Length > 0)
-            {
-                switch (args[0].ToLower())
-                {
-                    case "local":
-                        url = "https://lim-sc01/owa";
-                        break;
-
-                    case "externo":
-                        url = "https://mail.supervisorlinea2.com/owa";
-                        break;
-
-                    case "demo":
-                        url = "https://demo-sc.local/";
-                        break;
-
-                    default:
-                        url = args[0];
-                        break;
-                }
-            }
-
             Form form = new Form();
             form.Text = "Outlook Web - Supervisor Línea 2";
             form.WindowState = FormWindowState.Maximized;
@@ -68,44 +42,41 @@ namespace OWAHelper
             topBar.Dock = DockStyle.Top;
 
             Button btnLocal = new Button();
-            btnLocal.Text = "Correo Local";
+            btnLocal.Text = "Local";
             btnLocal.Left = 10;
             btnLocal.Top = 8;
 
             Button btnExterno = new Button();
-            btnExterno.Text = "Correo Externo";
-            btnExterno.Left = 120;
+            btnExterno.Text = "Externo";
+            btnExterno.Left = 100;
             btnExterno.Top = 8;
 
-            Button btnDemo = new Button();
-            btnDemo.Text = "Demo";
-            btnDemo.Left = 250;
-            btnDemo.Top = 8;
+            Button btnTest = new Button();
+            btnTest.Text = "Test";
+            btnTest.Left = 200;
+            btnTest.Top = 8;
 
             Button btnSalir = new Button();
             btnSalir.Text = "Salir";
-            btnSalir.Left = 330;
+            btnSalir.Left = 290;
             btnSalir.Top = 8;
 
             browser = new WebBrowser();
             browser.Dock = DockStyle.Fill;
             browser.ScriptErrorsSuppressed = true;
 
-            btnLocal.Click += (s, e) => browser.Navigate("https://lim-sc01/owa");
-            btnExterno.Click += (s, e) => browser.Navigate("https://mail.supervisorlinea2.com/owa");
-            btnDemo.Click += (s, e) => browser.Navigate("https://demo-sc.local/");
-            btnSalir.Click += (s, e) => form.Close();
+            btnLocal.Click += (s,e) => browser.Navigate("https://lim-sc01/owa");
+            btnExterno.Click += (s,e) => browser.Navigate("https://mail.supervisorlinea2.com/owa");
+            btnTest.Click += (s,e) => browser.Navigate("https://demo-sc.local/");
+            btnSalir.Click += (s,e) => form.Close();
 
             topBar.Controls.Add(btnLocal);
             topBar.Controls.Add(btnExterno);
-            topBar.Controls.Add(btnDemo);
+            topBar.Controls.Add(btnTest);
             topBar.Controls.Add(btnSalir);
 
             form.Controls.Add(browser);
             form.Controls.Add(topBar);
-
-            if (url != null)
-                browser.Navigate(url);
 
             Application.Run(form);
         }
