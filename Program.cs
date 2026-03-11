9f974b8f81d10f8322398d66507657e7cfcff81c
mnino80-rgb <mnino80@gmail.com>
2026-03-11 07:35:58 -0500

    Refactor Program.cs for improved structure and clarity

--- Program.cs
+++ Program.cs
@@ -1,87 +1,113 @@
 using System;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
 
-class Program
+namespace OWAHelper
 {
-    static WebBrowser browser;
-
-    [STAThread]
-    static void Main(string[] args)
+    static class Program
     {
-        Application.EnableVisualStyles();
+        static WebBrowser browser;
 
-        string url = null;
-
-        if (args.Length > 0)
+        static void SetBrowserFeatureControl()
         {
-            switch (args[0].ToLower())
+            try
             {
-                case "local":
-                    url = "https://lim-sc01/owa";
-                    break;
+                string appName = Path.GetFileName(Application.ExecutablePath);
 
-                case "externo":
-                    url = "https://mail.supervisorlinea2.com/owa";
-                    break;
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(
+                @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION");
 
-                case "demo":
-                    url = "https://demo-sc.local/";
-                    break;
-
-                default:
-                    url = args[0];
-                    break;
+                if (key != null)
+                {
+                    key.SetValue(appName, 11001, RegistryValueKind.DWord);
+                    key.Close();
+                }
             }
+            catch { }
         }
 
-        Form form = new Form();
-        form.Text = "Outlook Web - Supervisor Línea 2";
-        form.WindowState = FormWindowState.Maximized;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            SetBrowserFeatureControl();
 
-        Panel topBar = new Panel();
-        topBar.Height = 40;
-        topBar.Dock = DockStyle.Top;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-        Button btnLocal = new Button();
-        btnLocal.Text = "Correo Local";
-        btnLocal.Left = 10;
-        btnLocal.Top = 8;
+            string url = null;
 
-        Button btnExterno = new Button();
-        btnExterno.Text = "Correo Externo";
-        btnExterno.Left = 120;
-        btnExterno.Top = 8;
+            if (args.Length > 0)
+            {
+                switch (args[0].ToLower())
+                {
+                    case "local":
+                        url = "https://lim-sc01/owa";
+                        break;
 
-        Button btnDemo = new Button();
-        btnDemo.Text = "Demo";
-        btnDemo.Left = 250;
-        btnDemo.Top = 8;
+                    case "externo":
+                        url = "https://mail.supervisorlinea2.com/owa";
+                        break;
 
-        Button btnSalir = new Button();
-        btnSalir.Text = "Salir";
-        btnSalir.Left = 330;
-        btnSalir.Top = 8;
+                    case "demo":
+                        url = "https://demo-sc.local/";
+                        break;
 
-        browser = new WebBrowser();
-        browser.Dock = DockStyle.Fill;
-        browser.ScriptErrorsSuppressed = true;
+                    default:
+                        url = args[0];
+                        break;
+                }
+            }
 
-        btnLocal.Click += (s,e) => browser.Navigate("https://lim-sc01/owa");
-        btnExterno.Click += (s,e) => browser.Navigate("https://mail.supervisorlinea2.com/owa");
-        btnDemo.Click += (s,e) => browser.Navigate("https://demo-sc.local/");
-        btnSalir.Click += (s,e) => form.Close();
+            Form form = new Form();
+            form.Text = "Outlook Web - Supervisor Línea 2";
+            form.WindowState = FormWindowState.Maximized;
 
-        topBar.Controls.Add(btnLocal);
-        topBar.Controls.Add(btnExterno);
-        topBar.Controls.Add(btnDemo);
-        topBar.Controls.Add(btnSalir);
+            Panel topBar = new Panel();
+            topBar.Height = 40;
+            topBar.Dock = DockStyle.Top;
 
-        form.Controls.Add(browser);
-        form.Controls.Add(topBar);
+            Button btnLocal = new Button();
+            btnLocal.Text = "Correo Local";
+            btnLocal.Left = 10;
+            btnLocal.Top = 8;
 
-        if (url != null)
-            browser.Navigate(url);
+            Button btnExterno = new Button();
+            btnExterno.Text = "Correo Externo";
+            btnExterno.Left = 120;
+            btnExterno.Top = 8;
 
-        Application.Run(form);
+            Button btnDemo = new Button();
+            btnDemo.Text = "Demo";
+            btnDemo.Left = 250;
+            btnDemo.Top = 8;
+
+            Button btnSalir = new Button();
+            btnSalir.Text = "Salir";
+            btnSalir.Left = 330;
+            btnSalir.Top = 8;
+
+            browser = new WebBrowser();
+            browser.Dock = DockStyle.Fill;
+            browser.ScriptErrorsSuppressed = true;
+
+            btnLocal.Click += (s, e) => browser.Navigate("https://lim-sc01/owa");
+            btnExterno.Click += (s, e) => browser.Navigate("https://mail.supervisorlinea2.com/owa");
+            btnDemo.Click += (s, e) => browser.Navigate("https://demo-sc.local/");
+            btnSalir.Click += (s, e) => form.Close();
+
+            topBar.Controls.Add(btnLocal);
+            topBar.Controls.Add(btnExterno);
+            topBar.Controls.Add(btnDemo);
+            topBar.Controls.Add(btnSalir);
+
+            form.Controls.Add(browser);
+            form.Controls.Add(topBar);
+
+            if (url != null)
+                browser.Navigate(url);
+
+            Application.Run(form);
+        }
     }
 }
